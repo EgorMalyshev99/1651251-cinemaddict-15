85dff61ea7894ff5ee1181d03e9de4146913ca54
EgorMalyshev99 <egor.mlshv99@mail.ru>
2021-08-04 19:00:09 +0300

    alpha

---
+++ src/view/list.js
@@ -0,0 +1,5 @@
+// import {
+//   createComponent
+// } from "../main";
+
+// createComponent();
---
+++ src/view/menu.js
@@ -0,0 +1,20 @@
+import {
+  createComponent
+} from "../main";
+
+const mainWrap = document.querySelector('.main');
+const mainNav = document.createElement('nav');
+mainNav.className = 'main-navigation';
+mainNav.innerHTML = `
+  <div class="main-navigation__items">
+    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+  </div>
+  <a href="#stats" class="main-navigation__additional">Stats</a>
+`
+
+let position = 'start';
+
+createComponent(mainWrap, mainNav, position);
